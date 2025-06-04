a569be93ca8f25be00e9de1578bc0153fbb561aa
AhmedAirf <ahmed.airf.x@gmail.com>
2025-06-04 05:02:08 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-import { useEffect, useState } from "react";
 
 const App = () => {
   return (
